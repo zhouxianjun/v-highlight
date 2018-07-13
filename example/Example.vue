0f39e60d28dad6ff8ac6c1ccc40504fb3f29d20c
Alone <715213471@qq.com>
2018-07-13 14:33:57 +0800

    fix: 修复默认没有关键字异常

--- example/Example.vue
+++ example/Example.vue
@@ -5,8 +5,6 @@
             <button @click="add">新增</button>
             <input style="width:100px;margin-right:10px;" v-for="(w, index) in words" :key="index" type="text" v-model="words[index]" />
         </div>
-        <label>原文：</label>
-        <textarea rows="10" cols="50"></textarea>
         <p v-highlight="{keyword: words}">Tropical birds scattered as Drake veered the Jeep onto an old rutted track, snapping branches and tearing away vines, plowing through the rain forest with killers in pursuit, bullets flying, a gorgeous but pouty girl in the passenger’s seat, and a bitch of a headache. With only one of his arms on the wheel, the Jeep slewed to the left, and the pouty girl screamed as he forced the vehicle back onto the trail just before they would have crashed into a felled tree.</p>
     </div>
 </template>
@@ -17,7 +15,7 @@ export default {
     name: 'example',
     data () {
         return {
-            words: ['a']
+            words: []
         };
     },
     directives: {
--- src/highlight.js
+++ src/highlight.js
@@ -4,21 +4,27 @@ class Highlight {
         this.el = el;
         this.key = `mark-${uuid()}`;
     }
-    updateConfig (binding) {
+    validateCfg (binding) {
         let cfg = Object.assign(this.cfg || {caseSensitive: false, class: []}, binding.value);
         if (!cfg.keyword || !cfg.keyword.length) {
-            throw new Error('keyword must be not null');
+            return false;
         }
         cfg.class = Array.from(cfg.class);
         cfg.keyword = Array.from(cfg.keyword).filter(k => k.length);
+        if (!cfg.keyword.length) {
+            return false;
+        }
         // 最小的关键字长度(减少不必要的匹配)
         cfg.minLength = Math.max(...cfg.keyword.map(word => word ? word.length : 0));
         // 生成正则
         cfg.regex = new RegExp(`(${cfg.keyword.map(word => word.replace(/[-\\^$*+?.()|[\]{}/]/g, '\\$&')).join('|')})`, cfg.caseSensitive ? 'g' : 'ig');
         this.cfg = cfg;
+        return true;
     }
     apply (binding) {
-        this.updateConfig(binding);
+        if (!this.validateCfg(binding)) {
+            return;
+        }
         this.findNodes().filter(node => node.textContent.length >= this.cfg.minLength).forEach(node => {
             let mark = document.createElement('mark');
             mark.innerHTML = node.textContent.replace(this.cfg.regex, `<mark data-mark-key="${this.key}" class="${this.cfg.class.join(' ')}">$1</mark>`);
@@ -27,7 +33,9 @@ class Highlight {
         });
     }
     remove (binding) {
-        this.updateConfig(binding);
+        if (!this.validateCfg(binding)) {
+            return;
+        }
         this.el.querySelectorAll(`mark[data-mark-key=${this.key}]`).forEach(item => {
             let parent = item.parentNode;
             parent.replaceChild(item.firstChild, item);
